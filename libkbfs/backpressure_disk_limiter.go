4d302016b1eb40f0a6ab37aa397910e7c5b70bad
John Zila <john@keyba.se>
2017-03-10 11:27:46 -0800

    disk_limiter: First round of akalin-keybase's PR feedback

--- libkbfs/backpressure_disk_limiter.go
+++ libkbfs/backpressure_disk_limiter.go
@@ -493,7 +493,7 @@ func (bdl *backpressureDiskLimiter) beforeDiskBlockCachePut(
 	ctx context.Context, blockBytes, diskBlockCacheBytes int64) (
 	availableBytes int64, err error) {
 	if blockBytes == 0 {
-		// Better to return an error than to panic in Acquire.
+		// Better to return an error than to panic in ForceAcquire.
 		return 0, errors.New("backpressureDiskLimiter.beforeDiskBlockCachePut" +
 			" called with 0 blockBytes")
 	}
@@ -501,7 +501,7 @@ func (bdl *backpressureDiskLimiter) beforeDiskBlockCachePut(
 	defer bdl.lock.Unlock()
 	freeBytes, freeFiles, err := bdl.freeBytesAndFilesFn()
 	if err != nil {
-		return
+		return 0, err
 	}
 	bdl.byteTracker.updateFree(freeBytes)
 	bdl.fileTracker.updateFree(freeFiles)
